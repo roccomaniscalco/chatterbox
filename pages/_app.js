5cc2b58579c42a1843b9fd8545abe07d8b3ce084
Rocco Maniscalco <roccomaniscalco2001@gmail.com>
2022-08-05 00:24:39 -0400

    change MyApp to App
    • force vercel to build and redeploy :P

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import "../styles/globals.css";
 import { SessionProvider } from "next-auth/react";
 import Head from "next/head";
 
-function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+function App({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
       <Head>
@@ -13,4 +13,4 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   );
 }
 
-export default MyApp;
+export default App;
