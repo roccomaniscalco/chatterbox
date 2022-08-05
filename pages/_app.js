22968e90af446d3c88e27fde98522440a88e53b8
Rocco Maniscalco <roccomaniscalco2001@gmail.com>
2022-08-05 00:16:13 -0400

    add global document title

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
 import "../styles/globals.css";
 import { SessionProvider } from "next-auth/react";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
+      <Head>
+        <title>chatterbox</title>
+      </Head>
       <Component {...pageProps} />
     </SessionProvider>
   );
