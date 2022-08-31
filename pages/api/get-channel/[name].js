7ab3f4bfeee4caa7dad5e4e893bbb70ce7831870
Rocco Maniscalco <roccomaniscalco2001@gmail.com>
2022-08-31 02:54:51 -0400

    update schema

--- pages/api/get-channel/[name].js
+++ pages/api/get-channel/[name].js
@@ -4,14 +4,12 @@ const prisma = new PrismaClient();
 
 const getChannelHandler = async (req, res) => {
   const { name } = req.query;
-  console.log("name", name);
 
   try {
     const channel = await prisma.channel.findUniqueOrThrow({
       where: { name },
     });
     res.json(channel);
-    console.log("channel", channel);
   } catch (error) {
     console.error(error);
     res.status(404).json({ message: "Channel not found" });
