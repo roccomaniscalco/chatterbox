2037de5b7cebcdffd7df4f15db09eab90767e5da
Rocco Maniscalco <roccomaniscalco2001@gmail.com>
2022-09-13 19:37:22 -0400

    make TabHeader sticky

--- chatLayout/chatNavbar/TabHeader.jsx
+++ chatLayout/chatNavbar/TabHeader.jsx
@@ -1,19 +1,32 @@
-import { Group, Title } from "@mantine/core";
+import { Box, Group, Title } from "@mantine/core";
 import { node, string } from "prop-types";
 import React from "react";
 
 const TabHeader = ({ title, actionItem }) => {
   return (
-    <Group
-      mt="xs"
-      mx="md"
-      mb="sm"
-      position="apart"
-      sx={{ height: 44, display: "flex", alignItems: "center" }}
-    >
-      <Title order={4}>{title}</Title>
-      {actionItem}
-    </Group>
+    <Box
+      pt="xs"
+      px="md"
+      pb="xs"
+      sx={(theme) => ({
+        position: "sticky",
+        top: 0,
+        zIndex: 99,
+        backgroundColor: theme.colorScheme === "dark" ? theme.colors.dark[8] : theme.colors.gray[0],
+      })}
+    > 
+      <Group
+        position="apart"
+        sx={{
+          height: 44,
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        <Title order={4}>{title}</Title>
+        {actionItem}
+      </Group>
+    </Box>
   );
 };
 
