706da2fa5970fd1d63de284112f682c0219ecd58
Rocco Maniscalco <roccomaniscalco2001@gmail.com>
2022-10-02 02:45:33 -0400

    improve look and feel of user search

--- chatLayout/chatNavbar/friends/FriendsTab.jsx
+++ chatLayout/chatNavbar/friends/FriendsTab.jsx
@@ -1,6 +1,14 @@
-import { Autocomplete, Stack } from "@mantine/core";
+import {
+  ActionIcon,
+  Autocomplete,
+  Avatar,
+  Group,
+  Loader,
+  Stack,
+  Text,
+} from "@mantine/core";
 import { useDebouncedValue } from "@mantine/hooks";
-import { IconUserPlus } from "@tabler/icons";
+import { IconPlus, IconUserPlus, IconUserSearch } from "@tabler/icons";
 import { useQuery } from "@tanstack/react-query";
 import { useState } from "react";
 import AppModal from "../../../components/AppModal";
@@ -31,14 +39,39 @@ const FriendsTab = () => {
         <AppModal Icon={IconUserPlus} title="Add Friends">
           <Stack>
             <Autocomplete
-              data={users}
-              label="Search users"
-              
+              data={searchTerm.length > 0 ? users : []} // clear options when input is empty
+              label="Search chatters"
+              icon={<IconUserSearch />}
+              rightSection={
+                // show loading indicator when user is typing or fetching data
+                isLoading && searchTerm.length > 0 && <Loader size="sm" />
+              }
               data-autoFocus
               value={searchTerm}
               onChange={handleSearchTermChange}
-              nothingFound={searchTerm.length > 0 && !isLoading && "No users found"}
+              nothingFound={
+                // show nothing found message when input is not empty and there are no results
+                searchTerm.length > 0 && !isLoading && "No chatters found"
+              }
               filter={() => true} // disable filtering since it's done on the server
+              itemComponent={({ name, email, image, id }) => (
+                <div>
+                  <Group position="apart" spacing="xs" noWrap>
+                    <Group spacing="xs" noWrap>
+                      <Avatar src={image}  />
+                      <div>
+                        <Text size="sm" lineClamp={1}>{name}</Text>
+                        <Text size="xs" color="dimmed" lineClamp={1}>
+                          {email}
+                        </Text>
+                      </div>
+                    </Group>
+                    <ActionIcon variant="default" mr={6}>
+                      <IconPlus />
+                    </ActionIcon>
+                  </Group>
+                </div>
+              )}
             />
           </Stack>
         </AppModal>
