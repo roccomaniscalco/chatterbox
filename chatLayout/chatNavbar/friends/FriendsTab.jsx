3eb279d3632010f193fadd3b807ad0ed3391ca7d
Rocco Maniscalco <roccomaniscalco2001@gmail.com>
2022-10-03 04:15:28 -0400

    refactor to use less hacky jsx

--- chatLayout/chatNavbar/friends/FriendsTab.jsx
+++ chatLayout/chatNavbar/friends/FriendsTab.jsx
@@ -1,14 +1,14 @@
 import {
-  ActionIcon,
-  Autocomplete,
   Avatar,
+  Button,
   Group,
   Loader,
   Stack,
   Text,
+  TextInput,
 } from "@mantine/core";
 import { useDebouncedValue } from "@mantine/hooks";
-import { IconPlus, IconUserPlus, IconUserSearch } from "@tabler/icons";
+import { IconUserPlus, IconUserSearch } from "@tabler/icons";
 import { useQuery } from "@tanstack/react-query";
 import { useState } from "react";
 import AppModal from "../../../components/AppModal";
@@ -17,18 +17,24 @@ import TabHeader from "../TabHeader";
 
 const FriendsTab = () => {
   const [searchTerm, setSearchTerm] = useState("");
-  const [debouncedSearchTerm] = useDebouncedValue(searchTerm, 400);
+  const [debouncedSearchTerm, setDebouncedSearchTerm] = useDebouncedValue(
+    searchTerm,
+    400
+  );
 
-  const { data: users = [], isLoading } = useQuery(
+  const { data: users, isFetching } = useQuery(
     ["searchUsers", debouncedSearchTerm],
     () => api.searchUsers(debouncedSearchTerm),
     {
+      initialData: [],
       enabled: debouncedSearchTerm.length > 0,
-      select: (data) => data.map((user) => ({ value: user.name, ...user })),
     }
   );
 
-  const handleSearchTermChange = (value) => {
+  const isDebouncing = searchTerm !== debouncedSearchTerm;
+  const handleSearchTermChange = (event) => {
+    const value = event.currentTarget.value;
+    if (value === "") setDebouncedSearchTerm(value);
     setSearchTerm(value);
   };
 
@@ -38,41 +44,36 @@ const FriendsTab = () => {
       actionItem={
         <AppModal Icon={IconUserPlus} title="Add Friends">
           <Stack>
-            <Autocomplete
-              data={searchTerm.length > 0 ? users : []} // clear options when input is empty
-              label="Search chatters"
+            <TextInput
+              placeholder="Search for friends to add..."
               icon={<IconUserSearch />}
               rightSection={
-                // show loading indicator when user is typing or fetching data
-                isLoading && searchTerm.length > 0 && <Loader size="sm" />
+                (isFetching || isDebouncing) && <Loader size="sm" />
               }
-              data-autoFocus
+              data-autofocus
+              autoComplete="off"
               value={searchTerm}
               onChange={handleSearchTermChange}
-              nothingFound={
-                // show nothing found message when input is not empty and there are no results
-                searchTerm.length > 0 && !isLoading && "No chatters found"
-              }
-              filter={() => true} // disable filtering since it's done on the server
-              itemComponent={({ name, email, image, id }) => (
-                <div>
-                  <Group position="apart" spacing="xs" noWrap>
-                    <Group spacing="xs" noWrap>
-                      <Avatar src={image}  />
-                      <div>
-                        <Text size="sm" lineClamp={1}>{name}</Text>
-                        <Text size="xs" color="dimmed" lineClamp={1}>
-                          {email}
-                        </Text>
-                      </div>
-                    </Group>
-                    <ActionIcon variant="default" mr={6}>
-                      <IconPlus />
-                    </ActionIcon>
-                  </Group>
-                </div>
-              )}
             />
+
+            {users.map((user) => (
+              <Group position="apart" spacing="xs" noWrap key={user.id}>
+                <Group spacing="xs" noWrap>
+                  <Avatar src={user.image} />
+                  <div>
+                    <Text size="sm" lineClamp={1}>
+                      {user.name}
+                    </Text>
+                    <Text size="xs" color="dimmed" lineClamp={1} mt={-2}>
+                      {user.email}
+                    </Text>
+                  </div>
+                </Group>
+                <Button variant="filled" color compact>
+                  Request
+                </Button>
+              </Group>
+            ))}
           </Stack>
         </AppModal>
       }
