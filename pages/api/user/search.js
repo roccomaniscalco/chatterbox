34cfeef86b0039a0478876ebe2ddddccb86c0e80
Rocco Maniscalco <roccomaniscalco2001@gmail.com>
2022-10-05 04:58:23 -0400

    create api to upsert Friendship

--- pages/api/user/search.js
+++ pages/api/user/search.js
@@ -1,8 +1,11 @@
 import { PrismaClient } from "@prisma/client";
+import { unstable_getServerSession } from "next-auth";
+import { authOptions } from "../../../lib/auth";
 
 const prisma = new PrismaClient();
 
 export default async function userSearch(req, res) {
+  const session = await unstable_getServerSession(req, res, authOptions);
   const { searchTerm } = req.query;
 
   const users = await prisma.user.findMany({
@@ -12,12 +15,23 @@ export default async function userSearch(req, res) {
       name: true,
       image: true,
       email: true,
+      friendshipsReceived: {
+        where: {
+          senderId: session.user.id,
+        },
+      },
+      friendshipsSent: {
+        where: {
+          receiverId: session.user.id,
+        },
+      },
     },
     where: {
       OR: [
         { email: { contains: searchTerm } },
         { name: { contains: searchTerm } },
       ],
+      id: { not: session.user.id },
     },
     orderBy: {
       id: "asc",
---
+++ pages/api/user/upsertFriendship.js
@@ -0,0 +1,34 @@
+import { PrismaClient } from "@prisma/client";
+import { unstable_getServerSession } from "next-auth";
+import { authOptions } from "../../../lib/auth";
+
+const prisma = new PrismaClient();
+
+export default async function upsertFriendshipHandler(req, res) {
+  try {
+    const session = await unstable_getServerSession(req, res, authOptions);
+    const { receiverId, status } = req.body;
+
+    const friendship = await prisma.friendship.upsert({
+      where: {
+        senderId_receiverId: {
+          senderId: session.user.id,
+          receiverId,
+        },
+      },
+      create: {
+        senderId: session.user.id,
+        receiverId,
+        status,
+      },
+      update: {
+        status,
+      },
+    });
+
+    res.json(friendship);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Failed to create friend request" });
+  }
+}
